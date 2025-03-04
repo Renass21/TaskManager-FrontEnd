d66696f558855ce6c494a743abe013a7d351763e
renass21 <renatodossantoss21@gmail.com>
2025-03-04 10:57:45 -0300

    feat: add task component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
-import axios from 'axios'
-import React, { useState, useEffect } from 'react';
-import TaskItem from './components/TaskItem'
+import React from 'react';
+import Tasks from './components/Tasks'
 
 function App() {
-  const [tasks, setTasks] = useState([
-    { id: 1, description: 'Task 1', IsCompleted: false },
-    { id: 2, description: 'Task 2', description: true },
-  ])
-
-  const fetchTasks = async () => {
-    try {
-     const { data } = await axios.get('https://task-manager-backend-clqi.onrender.com/tasks')
-      setTasks(data)
-    } catch (error) {
-      console.log(error);
-      
-    }
-  }
  
-  useEffect(() => {
-    fetchTasks()
-  }, []); 
-  
-
   return (
     <>
-      <h2>Minhas Tarefas</h2>
-      <h3>Ultimas Tarefas</h3>
-      {tasks.map( task => 
-        <TaskItem key={task.id} task={task}/>
-      )} 
-      <h3>Tarefas Concluidas</h3>
+      < Tasks />
     </>
   )
 }
---
+++ src/components/Tasks.tsx
@@ -0,0 +1,58 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./Tasks.scss";
+import TaskItem from "./TaskItem";
+
+interface TaskProps {
+    id: number;
+    description: string;
+    isCompleted: boolean;
+}
+
+function Tasks(props: TaskProps) {
+    const [tasks, setTasks] = useState<TaskProps[]>([]);
+    
+
+      const fetchTasks = async () => {
+        try {
+         const { data } = await axios.get('https://task-manager-backend-clqi.onrender.com/tasks')
+         
+         const formatedTasks = data.map((task: any) => ({
+              id: task.id,
+              description: task.description,
+              isCompleted: task.is_completed,
+         }));
+
+         setTasks(formatedTasks);
+        } catch (error) {
+          console.log(error);
+          
+        }
+      }
+     
+      useEffect(() => {
+        fetchTasks()
+      }, []); 
+    return (
+      <div className="tasks-container">
+        <h2>Minhas Tarefas</h2>
+        <div className="last-tasks">
+            <h3>Últimas Tarefas</h3>
+            <div className="tasks-list">
+            {tasks.filter((task) => task.isCompleted === false).map((lastTask) => ( 
+            <TaskItem key={lastTask.id} task={lastTask} />))}
+            </div>
+        </div>
+        
+        <div className="completed-tasks">
+            <h3>Tarefas Concluídas</h3>
+            <div className="tasks-list">
+            {tasks.filter((task) => task.isCompleted === true).map((lastTask) => ( 
+            <TaskItem key={lastTask.id} task={lastTask} />))}
+            </div>
+        </div>
+      </div>
+    )
+}
+
+export default Tasks; 
